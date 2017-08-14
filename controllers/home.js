20edb61a1afbb4250551f5192a5fa4c664de8489
Greg Zhou <gregory.zhou@phsa.ca>
2017-08-14 12:17:53 -0700

    allow option to use qq

--- controllers/home.js
+++ controllers/home.js
@@ -6,16 +6,19 @@ const musicApi = require('music-api');
 
 exports.index = (req, res) => {
   let keywords = req.query.keywords;
+  let engine = req.query.engine;
   if(keywords){
     musicApi.searchSong('xiami',{
       key: keywords,
       limit:25,
       page:1,
+    
     }).then( ret => {
       let data = ret;
       res.render('home', {
         title: 'Home',
-        data
+        data,
+        engine
       });
     })
   }else{
@@ -28,7 +31,7 @@ res.render('home', {
 };
 exports.getSong= (req, res) => {
   let vendor = req.params.vendor;
-  musicApi.getSong('xiami', req.query || {})
+  musicApi.getSong(engine, req.query || {})
     .then(data => res.redirect(data.url))
     .catch(err => res.send(err))
 };
